9fd7095e66f7262836435cfb391b258812e47f5d
silyangel <8076550@qq.com>
2020-06-20 22:18:35 +0800

    data_change =>  some_data_change 用于 Messagecenter的消息

--- ones/lib/grid_view.js
+++ ones/lib/grid_view.js
@@ -114,7 +114,7 @@
                         try {
                             var mc = $injector.get('ones.MessageCenter');
                             console.debug('Try to connect message center');
-                            mc.on('data_changed', function(data) {
+                            mc.on('some_data_changed', function(data) {
                                 ones.DEBUG && console.debug('grid detcted data_changed event', data);
                                 if(data.app === self.app_info.app && data.module === self.app_info.module) {
                                     self.refresh();
